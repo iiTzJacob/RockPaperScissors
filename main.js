71636ced4ca842c11b15d32336c6f2e70c76d2a5
iiTzJacob <jj.colon7802@gmail.com>
2022-12-20 16:41:03 -0500

    Defined getComputerChoice function

---
+++ main.js
@@ -0,0 +1,9 @@
+function getComputerChoice() {
+    let choices = ["rock", "paper", "scissors"];
+
+    return choices.at((Math.random() * 3))
+}
+
+function playRound(playerSelection, computerSelection) {
+
+}
