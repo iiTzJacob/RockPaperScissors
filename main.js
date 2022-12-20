c0a69cf5bef71294e8344bc0af60a91986870e67
iiTzJacob <jj.colon7802@gmail.com>
2022-12-20 17:03:32 -0500

    Finished playRound and game() functions

--- main.js
+++ main.js
@@ -5,5 +5,24 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
+        return "You Lose! Rock beats Scissors!"
+    } else if (playerSelection.toLowerCase() == computerSelection) {
+        return `Tie game! You both chose ${playerSelection}.`
+    } else {
+        return `You win! ${playerSelection} beats ${computerSelection}.`
+    }
+}
 
+function game() {
+    for (let i = 0; i < 5; i++){
+        let playerSelection = prompt("Choose rock, paper, or scissors:");
+        let computerSelection = getComputerChoice();
+
+        alert(playRound(playerSelection, computerSelection))
+    }
 }
