12b5bbd31c5201b394a028e18a965fcad3691962
iiTzJacob <jj.colon7802@gmail.com>
2022-12-21 21:27:44 -0500

    Added buttons to html and added button function to main.js

--- main.js
+++ main.js
@@ -1,28 +1,42 @@
+const newDiv = document.createElement('div')
+const scores = document.querySelector('.scores')
+
+
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
 
     return choices.at((Math.random() * 3))
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
-        return "You Lose! Paper beats Rock"
-    } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
-        return "You Lose! Paper beats Rock"
-    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
-        return "You Lose! Rock beats Scissors!"
-    } else if (playerSelection.toLowerCase() == computerSelection) {
-        return `Tie game! You both chose ${playerSelection}.`
-    } else {
-        return `You win! ${playerSelection} beats ${computerSelection}.`
-    }
-}
 
-function game() {
-    for (let i = 0; i < 5; i++){
-        let playerSelection = prompt("Choose rock, paper, or scissors:");
-        let computerSelection = getComputerChoice();
+const btn = document.querySelectorAll('button');
 
-        alert(playRound(playerSelection, computerSelection))
+btn.forEach(button => {
+    button.addEventListener('click', function(){
+        playRound(button.value);
+    })
+})
+
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+    let result = "";
+    let playerScore = 0;
+    let computerScore = 0;
+
+    
+    if ((playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")) {
+            result += `You win! ${playerSelection} beats ${computerSelection}.`
+            playerScore += 1;
+        } else if (playerSelection == computerSelection) {
+            result += "Its a tie."
+        } else {
+            result += "You lose."
+            computerScore += 1;
+        }
+        newDiv.textContent = `${result}
+        Your Score: ${playerScore};
+        Computer Score: ${computerScore}`;
     }
-}
+    scores.append(newDiv);
